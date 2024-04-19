e22086706a3c48bdced210515c26c51b4f678eca
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2024-04-19 14:38:11 +0000

    Deploying to gh-pages from @ TalyaMonya/rental_camp_car@f5fcaa1b1995ef988be626f42b237fc697ccb175 🚀

---
+++ assets/FavoritePage-13abbb94.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-adf564a4.js";function t(){return e.jsx("section",{children:e.jsx("p",{children:"Favorites page !!!!"})})}export{t as default};
--- assets/WelcomePage-b919c003.js
+++ assets/WelcomePage-b919c003.js
@@ -1,4 +1,4 @@
-import{u as t,N as r,j as e}from"./index-2a80c76d.js";const n="/rental_camp_car/assets/background-0a7006a3.jpeg",a=t.div`
+import{u as t,N as r,j as e}from"./index-adf564a4.js";const n="/rental_camp_car/assets/background-0a7006a3.jpeg",a=t.div`
     position: absolute;
     top: 50px;
     height: 100vh;
@@ -14,10 +14,10 @@ import{u as t,N as r,j as e}from"./index-2a80c76d.js";const n="/rental_camp_car/
     max-width: 660px;
     text-align: center;
     color: #000000;
-    margin-bottom: 120px;
+    margin-bottom: 125px;
 `,c=t.div`
 display: flex;
-    width: 350px;
+    width: 400px;
     padding: 5px 15px;
     background-color: ${o=>o.theme.colors.lightgrey};
     border-radius: 33px;
@@ -50,4 +50,4 @@ display: flex;
     :focus {
         background-color: ${o=>o.theme.colors.hoverRed};
     }
-`,l=()=>e.jsx(e.Fragment,{children:e.jsxs(a,{children:[e.jsx(i,{children:"Our company UkrCampers will help you choose Campers for active recreation and travel"}),e.jsx(c,{children:e.jsx(s,{children:"“The world is a book, and those who do not travel have time to read only the first page” - Art. Augustine."})}),e.jsx(d,{children:"Find your camper"})]})});function g(){return e.jsx("section",{children:e.jsx(l,{})})}export{g as default};
+`,l=()=>e.jsx(e.Fragment,{children:e.jsxs(a,{children:[e.jsx(i,{children:"Our company UkrCampers will help you choose Campers for active recreation and travel"}),e.jsx(c,{children:e.jsx(s,{children:"“The world is a book, and those who do not travel have time to read only the first page” - Art. Augustine."})}),e.jsx(d,{to:"/catalog",children:"Find your camper"})]})});function g(){return e.jsx("section",{children:e.jsx(l,{})})}export{g as default};

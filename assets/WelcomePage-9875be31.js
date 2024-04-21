193fef73702d797c704f54f7d82ec6735a43b101
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2024-04-21 18:51:01 +0000

    Deploying to gh-pages from @ TalyaMonya/rental_camp_car@90cbb5662850b8025da42408f3815557f75847c7 🚀

--- assets/WelcomePage-9875be31.js
+++ assets/WelcomePage-9875be31.js
@@ -1,15 +1,11 @@
-import{u as t,N as r,j as e}from"./index-24962ecc.js";const n="/rental_camp_car/assets/background-0a7006a3.jpeg",a=t.div`
+import{u as t,N as r,j as e}from"./index-a83e4694.js";const n="/rental_camp_car/assets/background-0a7006a3.jpeg",a=t.div`
     position: absolute;
-    top: 50px;
     height: 100vh;
     width: 100%;
     background-image: url(${n});
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    margin: 0;
-    padding: 0 20px;
-    padding-top: 50px;
 `,i=t.h1`
     font-size: 35px;
     max-width: 660px;
@@ -23,12 +19,13 @@ display: flex;
     background-color: ${o=>o.theme.colors.lightgrey};
     border-radius: 33px;
     margin-bottom: 70px;
+    margin-left: 20px;
 `,s=t.p`
     font-size: 22px;
     text-align: center;
     color: ${o=>o.theme.colors.black};
     
-`,d=t(r)`
+`,l=t(r)`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -51,4 +48,4 @@ display: flex;
     :focus {
         background-color: ${o=>o.theme.colors.hoverRed};
     }
-`,l=()=>e.jsx(e.Fragment,{children:e.jsxs(a,{children:[e.jsx(i,{children:"Our company UkrCampers will help you choose Campers for active recreation and travel"}),e.jsx(c,{children:e.jsx(s,{children:"“The world is a book, and those who do not travel have time to read only the first page” - Art. Augustine."})}),e.jsx(d,{to:"/catalog",children:"Find your camper"})]})});function g(){return e.jsx("section",{children:e.jsx(l,{})})}export{g as default};
+`,d=()=>e.jsx(e.Fragment,{children:e.jsxs(a,{children:[e.jsx(i,{children:"Our company UkrCampers will help you choose Campers for active recreation and travel"}),e.jsx(c,{children:e.jsx(s,{children:"“The world is a book, and those who do not travel have time to read only the first page” - Art. Augustine."})}),e.jsx(l,{to:"/catalog",children:"Find your camper"})]})});function g(){return e.jsx("section",{children:e.jsx(d,{})})}export{g as default};
